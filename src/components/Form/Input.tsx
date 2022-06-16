6f53e95e82fd5d9c6735443e179064dbf6313773
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 09:03:46 -0400

    initial commit

--- src/components/Form/Input.tsx
+++
@@ -1,9 +0,0 @@
-const Input = ({ disabled = false, className, ...props }: any) => (
-    <input
-        disabled={disabled}
-        className={`${className} rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50`}
-        {...props}
-    />
-)
-
-export default Input
